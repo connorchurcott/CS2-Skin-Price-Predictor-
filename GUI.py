70d4faf14976e5233bd619c2521c09d66bc7bb94
connorchurcott <connorchurcott@gmail.com>
2026-02-23 15:00:38 -0800

    added output boxes, changed formatting to use grid for better layout

--- GUI.py
+++ GUI.py
@@ -5,11 +5,13 @@ from tkinter import ttk
 class GUIWindow(): 
 
     # Use GUIWindow.getSelectedValues() to get an array of the user entered values in this order [Wear, Rarity, WeaponType, isStattrak]
+    # Use GUIWindow.setPredictedValue(newPredictedValue) to set the GUI to display the new precited value
 
     def __init__(self):
         self.root = tk.Tk()
         self.windowStyling()
         self.setupInputFields()
+        self.setupOutputFields()
 
     def windowStyling(self): 
         self.root.title("CS2 Skin Price Predictor")
@@ -21,12 +23,12 @@ class GUIWindow():
         self.selectedWear = tk.StringVar()
         self.selectWearBox = ttk.Combobox(self.root, textvariable=self.selectedWear, state="readonly") 
         self.selectWearBox["values"] = ["Factory New (FN)", "Minimal Wear (MW)", "Field-Tested (FT)", "Well-Worn (WW)", "Battle-Scared (BS)"]
-        self.selectWearBox.pack(pady=10)
+        self.selectWearBox.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
         self.selectedRarity = tk.StringVar()
         self.selectRarityBox = ttk.Combobox(self.root, textvariable=self.selectedRarity, state="readonly")
         self.selectRarityBox["values"] = ["Consumer Grade (White)", "Industrial Grade (light blue)", "Mil-Spec (blue)", "Restricted (Purple)", "Classified (Pink)", "Covert (Red)", "Contraband (Yellow)"]
-        self.selectRarityBox.pack(pady=10)
+        self.selectRarityBox.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 
         self.selectedWeaponType = tk.StringVar()
         self.selectWeaponTypeBox = ttk.Combobox(self.root, textvariable=self.selectedWeaponType, state="readonly")
@@ -35,16 +37,29 @@ class GUIWindow():
                                               "---SMGs---", "MAC-10", "MP5-SD", "MP7", "MP9", "PP-Bizon", "P90", "UMP-45",
                                               "---HEAVY---", "MAG-7", "Nova", "Sawed-Off", "XM1014", "M249", "Negev", 
                                               "---KNIVES---", "Bayonet", "Bowie Knife", "Butterfly Knife", "Classic Knife", "Falchion Knife", "Flip Knife", "Gut Knife", "Huntsman Knife", "Karambit", "Kukri Knife", "M9 Bayonet", "Navaja Knife", "Nomad Knife", "Paracord Knife", "Shadow Daggers", "Skeleton Knife", "Stiletto Knife", "Survival Knife", "Talon Knife", "Ursus Knife"]
-        self.selectWeaponTypeBox.pack(pady=10)
+        self.selectWeaponTypeBox.grid(row=2, column=0, padx=10, pady=10, sticky="w")
 
         self.selectedStatTrak = tk.StringVar()
         self.selectStatTrakBox = ttk.Combobox(self.root, textvariable=self.selectedStatTrak, state="readonly")
         self.selectStatTrakBox["values"] = ["Yes", "No"]
-        self.selectStatTrakBox.pack(pady=10)
+        self.selectStatTrakBox.grid(row=3, column=0, padx=10, pady=10, sticky="w")
 
-        self.button = tk.Button(self.root, text="print selection", command=self.printselected)
-        self.button.pack(); 
 
+        # replace self.printselected with actual function that sends that to the predictor later
+        self.button = tk.Button(self.root, text="Submit", command=self.printselected)
+        self.button.grid(row=4, column=0, pady=10)
+    
+    def setupOutputFields(self): 
+        self.predictedValue = tk.DoubleVar()
+        self.outputLabel = tk.Label(self.root, text="Predicted Value: ")
+        self.outputLabel.grid(row=5, column=0, padx=10, pady=50, sticky="w")
+        self.outputBox = tk.Label(self.root, textvariable=self.predictedValue, relief="sunken", width=15)
+        self.outputBox.grid(row=5, column=1, padx=1, pady=10, sticky="w")
+
+
+
+    def setPredictedValue(self, newPredictedValue): 
+        self.predictedValue.set(newPredictedValue)  
 
     def getSelectedValues(self): 
         selectedValues = [self.selectedWear.get(), self.selectedRarity.get(), self.selectedWeaponType.get(), self.selectedStatTrak.get()]
@@ -58,6 +73,7 @@ class GUIWindow():
 
     
     def printselected(self):
+        self.setPredictedValue(0.67) 
         print(self.getSelectedValues())
         return 
 
