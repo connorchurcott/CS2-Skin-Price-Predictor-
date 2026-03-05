9a43c47a09e0873637ee70e3a061a7e30fc54520
Jotez <raf.cortez12@gmail.com>
2026-03-05 12:54:29 -0800

    Added function that gets the price for the specific type of skin

--- data.py
+++ data.py
@@ -1,8 +1,18 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 DATA = "Skins_Price.csv"
 
+#Some values of the prices are labelled as Not Possible
 df = pd.read_csv(DATA)
 
-print(df.keys())
+
+def get_price(price):
+    #Returns a new dataframe with the chosen price.
+    #Price is divided between Factory New, Minimal Wear, Field-Tested, Well-Worn, Battle-Scarred
+    #And their StatTrak versions
+    final = df[['Weapon','Case','Rarity','Min Wear','Max Wear','Case_1',price]].copy(deep=True) 
+    return final
+
+
